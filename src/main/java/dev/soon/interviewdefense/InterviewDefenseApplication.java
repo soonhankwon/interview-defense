8004bfa7c7eef0060745c52de6f277af89322efd
soonhankwon <soonable@gmail.com>
2023-09-18 12:01:15 +0900

    Initial commit

---
+++ src/main/java/dev/soon/interviewdefense/InterviewDefenseApplication.java
@@ -0,0 +1,13 @@
+package dev.soon.interviewdefense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewDefenseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewDefenseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/soon/interviewdefense/InterviewDefenseApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.soon.interviewdefense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewDefenseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
