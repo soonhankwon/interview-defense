b1071ca1130549cda5c1256128b6799b4c1829bd
soonhankwon <soonable@gmail.com>
2023-09-19 20:05:06 +0900

    feat : 유저의 Chat 조회

--- src/main/java/dev/soon/interviewdefense/chat/respository/ChatRepository.java
+++ src/main/java/dev/soon/interviewdefense/chat/respository/ChatRepository.java
@@ -1,9 +1,13 @@
 package dev.soon.interviewdefense.chat.respository;
 
 import dev.soon.interviewdefense.chat.domain.Chat;
+import dev.soon.interviewdefense.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChatRepository extends JpaRepository<Chat, Long> {
+    List<Chat> findChatsByUser(User user);
 }
