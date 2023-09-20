870db9c9ed50d53a4b77f47f649486202e656beb
soonhankwon <soonable@gmail.com>
2023-09-20 23:18:46 +0900

    feat : GPT 라이브러리 변경

--- src/main/java/dev/soon/interviewdefense/chat/controller/ChatController.java
+++ src/main/java/dev/soon/interviewdefense/chat/controller/ChatController.java
@@ -1,15 +1,18 @@
 package dev.soon.interviewdefense.chat.controller;
 
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatCompletionResult;
+import com.theokanning.openai.service.OpenAiService;
 import dev.soon.interviewdefense.chat.controller.dto.ChatMessageDto;
 import dev.soon.interviewdefense.chat.controller.dto.ChatRoomReqDto;
 import dev.soon.interviewdefense.chat.domain.Chat;
 import dev.soon.interviewdefense.chat.domain.ChatMessage;
+import dev.soon.interviewdefense.chat.domain.ChatSender;
 import dev.soon.interviewdefense.chat.respository.ChatMessageRepository;
 import dev.soon.interviewdefense.chat.service.ChatService;
 import dev.soon.interviewdefense.security.SecurityUser;
 import dev.soon.interviewdefense.user.domain.User;
 import dev.soon.interviewdefense.user.service.UserService;
-import io.github.flashvayne.chatgpt.service.ChatgptService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -24,11 +27,9 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/chat")
 public class ChatController {
-
-    private final ChatgptService chatgptService;
     private final ChatService chatService;
+    private final OpenAiService openAiService;
     private final UserService userServiceImpl;
-
     private final ChatMessageRepository chatMessageRepository;
 
     @GetMapping("/create")
@@ -49,9 +50,9 @@ public class ChatController {
     public String getChatRoom(@AuthenticationPrincipal SecurityUser securityUser,
                               @PathVariable Long chatRoomId, Model model) {
         Chat chatRoom = chatService.getChatRoom(securityUser, chatRoomId);
-        List<ChatMessage> chatMessagesByChat = chatMessageRepository.findChatMessagesByChat(chatRoom);
+        List<ChatMessage> chatMessagesInChatRoom = chatMessageRepository.findChatMessagesByChat(chatRoom);
 
-        model.addAttribute("chatMessages", chatMessagesByChat);
+        model.addAttribute("chatMessages", chatMessagesInChatRoom);
         model.addAttribute("chat", chatRoom);
         User loginUserInfo = userServiceImpl.getLoginUserInfo(securityUser);
         model.addAttribute("user", loginUserInfo);
@@ -60,13 +61,30 @@ public class ChatController {
     }
 
     @PostMapping("/{chatRoomId}")
-    public String submitMessage(@PathVariable Long chatRoomId,
-                                @AuthenticationPrincipal SecurityUser securityUser,
-                                @ModelAttribute("chatMessageDto") ChatMessageDto dto) {
+    public String sendMessage(@PathVariable Long chatRoomId,
+                              @AuthenticationPrincipal SecurityUser securityUser,
+                              @ModelAttribute("chatMessageDto") ChatMessageDto dto) {
         log.info("dto={}", dto);
-        String res = chatgptService.sendMessage(dto.message());
-        chatService.saveMessage(chatRoomId, securityUser, res);
-        log.info("res={}", res);
+        chatService.saveMessage(chatRoomId, securityUser, dto.message(), ChatSender.USER);
+        ChatCompletionRequest request = ChatCompletionRequest.builder()
+                .model("gpt-3.5-turbo")
+                .messages(List.of(
+                        new com.theokanning.openai.completion.chat.ChatMessage("system", "당신은 자바 전문가입니다."),
+                        new com.theokanning.openai.completion.chat.ChatMessage("user", dto.message())
+                ))
+                .maxTokens(1000)
+                .build();
+        ChatCompletionResult chatCompletion = openAiService.createChatCompletion(request);
+        String response = chatCompletion.getChoices().get(0).getMessage().getContent();
+        log.info("response={}", response);
+        chatService.saveMessage(chatRoomId, securityUser, response, ChatSender.AI);
         return "redirect:/chat/{chatRoomId}";
+    }
+
+    @PostMapping("/{chatRoomId}/delete")
+    public String deleteChat(@PathVariable Long chatRoomId,
+                             @AuthenticationPrincipal SecurityUser securityUser) {
+        chatService.deleteChat(chatRoomId, securityUser);
+        return "redirect:/";
     }
 }
