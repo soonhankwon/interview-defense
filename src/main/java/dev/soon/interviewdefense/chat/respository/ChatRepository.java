a1bd49e916e033ceaa4e323915e15f8cb0f85035
soonhankwon <soonable@gmail.com>
2023-09-20 23:20:02 +0900

    feat : 유저의 채팅방 찾기 쿼리

--- src/main/java/dev/soon/interviewdefense/chat/respository/ChatRepository.java
+++ src/main/java/dev/soon/interviewdefense/chat/respository/ChatRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ChatRepository extends JpaRepository<Chat, Long> {
     List<Chat> findChatsByUser(User user);
+    Optional<Chat> findChatByUserAndId(User user, Long chatId);
 }
