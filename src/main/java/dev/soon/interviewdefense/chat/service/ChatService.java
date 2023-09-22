0d41720c10bdc054833fe8e36bb59579aafe8939
soonhankwon <soonable@gmail.com>
2023-09-22 18:38:00 +0900

    feat : 멘토링, 디펜스 채팅 기능 정의

--- src/main/java/dev/soon/interviewdefense/chat/service/ChatService.java
+++ src/main/java/dev/soon/interviewdefense/chat/service/ChatService.java
@@ -1,16 +1,32 @@
 package dev.soon.interviewdefense.chat.service;
 
+import dev.soon.interviewdefense.chat.controller.dto.ChatMessageDto;
 import dev.soon.interviewdefense.chat.controller.dto.ChatRoomReqDto;
+import dev.soon.interviewdefense.chat.controller.dto.DefenseChatRoomReqDto;
 import dev.soon.interviewdefense.chat.domain.Chat;
-import dev.soon.interviewdefense.chat.domain.ChatSender;
+import dev.soon.interviewdefense.chat.domain.ChatMessage;
 import dev.soon.interviewdefense.security.SecurityUser;
+
+import java.util.List;
 
 public interface ChatService {
     Chat getChatRoom(SecurityUser securityUser, Long chatRoomId);
 
     Long createChatRoom(SecurityUser securityUser, ChatRoomReqDto dto);
 
-    void saveMessage(Long chatRoomId, SecurityUser securityUser, String message, ChatSender sender);
+    Chat saveUserMessage(Long chatRoomId, SecurityUser securityUser, ChatMessageDto dto);
+
+    void saveAIMessage(Long chatRoomId, SecurityUser securityUser, String message);
+
+    String generatePrompt(Chat chat, ChatMessageDto dto);
+
+    String generateDefensePrompt(Chat chat, SecurityUser securityUser, ChatMessageDto dto);
 
     void deleteChat(Long chatRoomId, SecurityUser securityUser);
+
+    Long createDefenseChatRoom(SecurityUser securityUser, DefenseChatRoomReqDto dto);
+
+    String initDefensePrompt(SecurityUser securityUser, DefenseChatRoomReqDto dto);
+
+    List<ChatMessage> getChatRoomMessages(Chat chatRoom);
 }
