cc7bc443f227b950cf9122f0a348671c4956175c
soonhankwon <soonable@gmail.com>
2023-09-22 18:43:59 +0900

    feat : 마이페이지 컨트롤러

--- src/main/java/dev/soon/interviewdefense/web/MyPageController.java
+++ src/main/java/dev/soon/interviewdefense/web/MyPageController.java
@@ -1,17 +1,21 @@
 package dev.soon.interviewdefense.web;
 
 import dev.soon.interviewdefense.security.SecurityUser;
+import dev.soon.interviewdefense.user.domain.Language;
+import dev.soon.interviewdefense.user.domain.Tech;
 import dev.soon.interviewdefense.user.domain.User;
 import dev.soon.interviewdefense.user.service.UserService;
+import dev.soon.interviewdefense.web.dto.MyLanguageReqDto;
 import dev.soon.interviewdefense.web.dto.MyPageUpdateForm;
+import dev.soon.interviewdefense.web.dto.MyTechReqDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,14 +26,22 @@ public class MyPageController {
     @GetMapping("/myPage")
     public String myPage(@AuthenticationPrincipal SecurityUser securityUser, Model model) {
         User loginUserInfo = userService.getLoginUserInfo(securityUser);
+        List<Language> loginUserLanguages = userService.getLoginUserLanguages(securityUser);
+        List<Tech> loginUserTechs = userService.getLoginUserTechs(securityUser);
         model.addAttribute("user", loginUserInfo);
+        model.addAttribute("myLanguages", loginUserLanguages);
+        model.addAttribute("myTechs", loginUserTechs);
         return "myPage";
     }
 
     @GetMapping("/myPage/update")
     public String updateMyPage(@AuthenticationPrincipal SecurityUser securityUser, Model model) {
         User loginUserInfo = userService.getLoginUserInfo(securityUser);
+        List<Language> loginUserLanguages = userService.getLoginUserLanguages(securityUser);
+        List<Tech> loginUserTechs = userService.getLoginUserTechs(securityUser);
         model.addAttribute("user", loginUserInfo);
+        model.addAttribute("myLanguages", loginUserLanguages);
+        model.addAttribute("myTechs", loginUserTechs);
         return "myPageUpdateForm";
     }
 
@@ -38,5 +50,33 @@ public class MyPageController {
                        @ModelAttribute("user") MyPageUpdateForm form) {
         userService.updateMyPage(securityUser, form);
         return "redirect:/myPage";
+    }
+
+    @PostMapping("/myPage/languages/add")
+    public String addMyLanguage(@AuthenticationPrincipal SecurityUser securityUser,
+                                @RequestBody MyLanguageReqDto dto) {
+        userService.addMyLanguageInMyPage(securityUser, dto);
+        return "redirect:/myPage/update";
+    }
+
+    @PostMapping("/myPage/languages/{languageId}/delete")
+    public String deleteMyLanguage(@PathVariable Long languageId,
+                                   @AuthenticationPrincipal SecurityUser securityUser) {
+        userService.deleteMyLanguageInMyPage(securityUser, languageId);
+        return "redirect:/myPage/update";
+    }
+
+    @PostMapping("/myPage/techs/add")
+    public String addMyTech(@AuthenticationPrincipal SecurityUser securityUser,
+                            @RequestBody MyTechReqDto dto) {
+        userService.addMyTechInMyPage(securityUser, dto);
+        return "redirect:/myPage/update";
+    }
+
+    @PostMapping("/myPage/techs/{techId}/delete")
+    public String deleteMyTech(@PathVariable Long techId,
+                               @AuthenticationPrincipal SecurityUser securityUser) {
+        userService.deleteMyTechInMyPage(securityUser, techId);
+        return "redirect:/myPage/update";
     }
 }
