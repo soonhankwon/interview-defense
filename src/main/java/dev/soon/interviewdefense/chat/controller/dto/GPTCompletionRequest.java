8c224c7ed80213e1367576b6a8c569ca307b1158
soonhankwon <soonable@gmail.com>
2023-09-26 20:59:43 +0900

    feat : CompletionChatRequest DTO (#6)

---
+++ src/main/java/dev/soon/interviewdefense/chat/controller/dto/GPTCompletionRequest.java
@@ -0,0 +1,13 @@
+package dev.soon.interviewdefense.chat.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GPTCompletionRequest {
+
+    private String prompt;
+}
