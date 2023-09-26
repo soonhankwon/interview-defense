f27325b0a802f8b73bc3cb974e624681b1039496
soonhankwon <soonable@gmail.com>
2023-09-26 18:42:23 +0900

    refactor : Defense Service method 분리

--- src/main/java/dev/soon/interviewdefense/chat/service/ChatServiceV1.java
+++ src/main/java/dev/soon/interviewdefense/chat/service/ChatServiceV1.java
@@ -5,11 +5,9 @@ import com.theokanning.openai.completion.chat.ChatCompletionResult;
 import com.theokanning.openai.service.OpenAiService;
 import dev.soon.interviewdefense.chat.controller.dto.ChatMessageDto;
 import dev.soon.interviewdefense.chat.controller.dto.ChatRoomReqDto;
-import dev.soon.interviewdefense.chat.controller.dto.DefenseChatRoomReqDto;
 import dev.soon.interviewdefense.chat.domain.Chat;
 import dev.soon.interviewdefense.chat.domain.ChatMessage;
 import dev.soon.interviewdefense.chat.domain.ChatSender;
-import dev.soon.interviewdefense.chat.domain.ChatTopic;
 import dev.soon.interviewdefense.chat.respository.ChatMessageRepository;
 import dev.soon.interviewdefense.chat.respository.ChatRepository;
 import dev.soon.interviewdefense.security.SecurityUser;
@@ -21,8 +19,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import static dev.soon.interviewdefense.chat.util.PromptGenerator.*;
 
@@ -30,10 +26,6 @@ import static dev.soon.interviewdefense.chat.util.PromptGenerator.*;
 @RequiredArgsConstructor
 @Service
 public class ChatServiceV1 implements ChatService {
-
-    private static final String PATTERN = "점수: (\\d+)";
-    private static final Pattern SCORE_PATTERN = Pattern.compile(PATTERN);
-    private static final String DEFAULT_TOPIC_VALUE = "컴퓨터";
 
     private final ChatRepository chatRepository;
     private final UserRepository userRepository;
@@ -44,16 +36,6 @@ public class ChatServiceV1 implements ChatService {
     @Override
     @Transactional
     public Long createChatRoom(SecurityUser securityUser, ChatRoomReqDto dto) {
-        User user = userRepository.findUserByEmail(securityUser.getUsername())
-                .orElseThrow();
-        Chat chat = new Chat(dto, user);
-        chatRepository.save(chat);
-        return chat.getId();
-    }
-
-    @Override
-    @Transactional
-    public Long createDefenseChatRoom(SecurityUser securityUser, DefenseChatRoomReqDto dto) {
         User user = userRepository.findUserByEmail(securityUser.getUsername())
                 .orElseThrow();
         Chat chat = new Chat(dto, user);
@@ -99,12 +81,7 @@ public class ChatServiceV1 implements ChatService {
 
     @Override
     public String generatePrompt(Chat chat, ChatMessageDto dto) {
-        String topic;
-        if (chat.isTopicDefault()) {
-            topic = DEFAULT_TOPIC_VALUE;
-        } else {
-            topic = chat.getTopic().getValue();
-        }
+        String topic = chat.getTopic().getValue();
         String rolePrompt = MENTOR_ROLE_CHARACTER +
                 topic +
                 MENTOR_ROLE +
@@ -117,99 +94,6 @@ public class ChatServiceV1 implements ChatService {
                 dto.message());
         ChatCompletionResult chatCompletion = openAiService.createChatCompletion(request);
         return chatCompletion.getChoices().get(0).getMessage().getContent();
-    }
-
-    @Override
-    public String initDefensePrompt(SecurityUser securityUser, DefenseChatRoomReqDto dto) {
-        User user = userRepository.findUserByEmail(securityUser.getUsername()).orElseThrow();
-        StringBuilder sb = new StringBuilder();
-        user.getLanguages().forEach(i -> sb.append(i.getName().getValue()).append(","));
-        String userLanguages = sb.toString();
-        sb.setLength(0);
-
-        user.getTechs().forEach(i -> sb.append(i.getName().getValue()).append(","));
-        sb.append("기술스택 사용자. ");
-        String userTechs = sb.toString();
-        sb.setLength(0);
-        String topic;
-        if (dto.topic() == ChatTopic.DEFAULT) {
-            topic = "";
-        } else {
-            topic = dto.topic().getValue();
-        }
-        sb.append(INIT_DEFENSE_CHARACTER)
-                .append(topic)
-                .append(userLanguages)
-                .append(userTechs)
-                .append(user.getPosition())
-                .append(INIT_DEFENSE_ROLE)
-                .append(INIT_DEFENSE_ROLE_KOREAN)
-                .append(INIT_DEFENSE_LIMITED);
-        String rolePrompt = sb.toString();
-        sb.setLength(0);
-
-        sb.append(INIT_DEFENSE_USER_HELLO)
-                .append(userLanguages)
-                .append(userTechs)
-                .append(INIT_DEFENSE_USER_QUESTION);
-        String promptUser = sb.toString();
-
-        ChatCompletionRequest request = requestOpenAI(
-                new com.theokanning.openai.completion.chat.ChatMessage("system", rolePrompt),
-                promptUser);
-        ChatCompletionResult chatCompletion = openAiService.createChatCompletion(request);
-        return chatCompletion.getChoices().get(0).getMessage().getContent();
-    }
-
-    @Override
-    public String generateDefensePrompt(Chat chat, SecurityUser securityUser, ChatMessageDto dto) {
-        User user = userRepository.findUserByEmail(securityUser.getUsername()).orElseThrow();
-        StringBuilder sb = new StringBuilder();
-        user.getLanguages().forEach(i -> sb.append(i.getName().getValue()).append(","));
-        String userLanguages = sb.toString();
-        sb.setLength(0);
-        user.getTechs().forEach(i -> sb.append(i.getName().getValue()).append(","));
-        String userTechs = sb.toString();
-        sb.setLength(0);
-        sb.append(DEFENSE_CHARACTER)
-                .append(user.getPosition().getValue())
-                .append(DEFENSE_ROLE)
-                .append(DEFENSE_ROLE_SITUATION)
-                .append(DEFENSE_ROLE_SITUATION_OPTION)
-                .append(user.getPosition().getValue())
-                .append("입니다. 사용 기술스택은")
-                .append(userLanguages)
-                .append(userTechs)
-                .append(" 입니다.")
-                .append(DEFENSE_ROLE_QUESTION_OPTION)
-//                .append(DEFENSE_OPTION_ROLE_SCORE)
-                .append(DEFENSE_OPTION_SCORE)
-                .append(DEFENSE_OPTION_SCORE_UNNATURAL)
-                .append(DEFENSE_OPTION_QUESTION_FORBIDDEN_A)
-                .append(DEFENSE_OPTION_SCORE_SHORT)
-                .append(DEFENSE_OPTION_SCORE_DONT_KNOW)
-                .append(DEFENSE_OPTION_SCORE_GREAT)
-                .append(DEFENSE_OPTION_SCORE_BAD);
-//                .append(DEFENSE_OPTION_QUESTION_FORBIDDEN);
-        String rolePrompt = sb.toString();
-
-        ChatCompletionRequest request = requestOpenAI(new com.theokanning.openai.completion.chat.ChatMessage("system", rolePrompt), dto.message());
-        ChatCompletionResult chatCompletion = openAiService.createChatCompletion(request);
-        chat.decreaseAIQuestionMaxNumber();
-        if (chat.isDefenseEnd()) {
-            String feedbackUser = "지금까지의 대화를 전문가의 입장에서 평가해서 보고서를 만들어주세요. 인터뷰가 끝났습니다.";
-            ChatCompletionRequest request1 = requestOpenAI(new com.theokanning.openai.completion.chat.ChatMessage("system", rolePrompt), feedbackUser);
-            ChatCompletionResult chatCompletion1 = openAiService.createChatCompletion(request1);
-            return "면접 디펜스가 끝났습니다. 감사합니다." + "\n" + chatCompletion1.getChoices().get(0).getMessage().getContent();
-        }
-        String content = chatCompletion.getChoices().get(0).getMessage().getContent();
-        Matcher matcher = SCORE_PATTERN.matcher(content);
-        if (matcher.find()) {
-            String scoreStr = matcher.group(1);
-            int score = Integer.parseInt(scoreStr);
-            chat.increaseScore(score);
-        }
-        return content;
     }
 
     private ChatCompletionRequest requestOpenAI(com.theokanning.openai.completion.chat.ChatMessage rolePrompt, String userPrompt) {
--- src/main/java/dev/soon/interviewdefense/chat/service/ChatServiceV2.java
+++ src/main/java/dev/soon/interviewdefense/chat/service/ChatServiceV2.java
@@ -5,11 +5,9 @@ import com.theokanning.openai.completion.chat.ChatCompletionResult;
 import com.theokanning.openai.service.OpenAiService;
 import dev.soon.interviewdefense.chat.controller.dto.ChatMessageDto;
 import dev.soon.interviewdefense.chat.controller.dto.ChatRoomReqDto;
-import dev.soon.interviewdefense.chat.controller.dto.DefenseChatRoomReqDto;
 import dev.soon.interviewdefense.chat.domain.Chat;
 import dev.soon.interviewdefense.chat.domain.ChatMessage;
 import dev.soon.interviewdefense.chat.domain.ChatSender;
-import dev.soon.interviewdefense.chat.domain.ChatTopic;
 import dev.soon.interviewdefense.chat.respository.ChatMessageRepository;
 import dev.soon.interviewdefense.chat.respository.ChatRepository;
 import dev.soon.interviewdefense.security.SecurityUser;
@@ -21,9 +19,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import static dev.soon.interviewdefense.chat.util.PromptGenerator.*;
 
@@ -31,10 +26,6 @@ import static dev.soon.interviewdefense.chat.util.PromptGenerator.*;
 @RequiredArgsConstructor
 @Service
 public class ChatServiceV2 implements ChatService {
-
-    private static final String PATTERN = "점수: (\\d+)";
-    private static final Pattern SCORE_PATTERN = Pattern.compile(PATTERN);
-    private static final String DEFAULT_TOPIC_VALUE = "컴퓨터";
 
     private final ChatRepository chatRepository;
     private final UserRepository userRepository;
@@ -45,16 +36,6 @@ public class ChatServiceV2 implements ChatService {
     @Override
     @Transactional
     public Long createChatRoom(SecurityUser securityUser, ChatRoomReqDto dto) {
-        User user = userRepository.findUserByEmail(securityUser.getUsername())
-                .orElseThrow();
-        Chat chat = new Chat(dto, user);
-        chatRepository.save(chat);
-        return chat.getId();
-    }
-
-    @Override
-    @Transactional
-    public Long createDefenseChatRoom(SecurityUser securityUser, DefenseChatRoomReqDto dto) {
         User user = userRepository.findUserByEmail(securityUser.getUsername())
                 .orElseThrow();
         Chat chat = new Chat(dto, user);
@@ -100,12 +81,7 @@ public class ChatServiceV2 implements ChatService {
 
     @Override
     public String generatePrompt(Chat chat, ChatMessageDto dto) {
-        String topic;
-        if (chat.isTopicDefault()) {
-            topic = DEFAULT_TOPIC_VALUE;
-        } else {
-            topic = chat.getTopic().getValue();
-        }
+        String topic = chat.getTopic().getValue();
         String rolePrompt = MENTOR_ROLE_CHARACTER +
                 topic +
                 MENTOR_ROLE +
@@ -118,99 +94,6 @@ public class ChatServiceV2 implements ChatService {
                 dto.message());
         ChatCompletionResult chatCompletion = openAiService.createChatCompletion(request);
         return chatCompletion.getChoices().get(0).getMessage().getContent();
-    }
-
-    @Override
-    public String initDefensePrompt(SecurityUser securityUser, DefenseChatRoomReqDto dto) {
-        User user = userRepository.findUserByEmail(securityUser.getUsername()).orElseThrow();
-        StringBuilder sb = new StringBuilder();
-        user.getLanguages().forEach(i -> sb.append(i.getName().getValue()).append(","));
-        String userLanguages = sb.toString();
-        sb.setLength(0);
-
-        user.getTechs().forEach(i -> sb.append(i.getName().getValue()).append(","));
-        sb.append("기술스택 사용자. ");
-        String userTechs = sb.toString();
-        sb.setLength(0);
-        String topic;
-        if (dto.topic() == ChatTopic.DEFAULT) {
-            topic = "";
-        } else {
-            topic = dto.topic().getValue();
-        }
-        sb.append(INIT_DEFENSE_CHARACTER)
-                .append(topic)
-                .append(userLanguages)
-                .append(userTechs)
-                .append(user.getPosition().getValue())
-                .append(INIT_DEFENSE_ROLE)
-                .append(INIT_DEFENSE_ROLE_KOREAN)
-                .append(INIT_DEFENSE_LIMITED);
-        String rolePrompt = sb.toString();
-        sb.setLength(0);
-
-        sb.append(INIT_DEFENSE_USER_HELLO)
-                .append(userLanguages)
-                .append(userTechs)
-                .append(INIT_DEFENSE_USER_QUESTION);
-        String promptUser = sb.toString();
-
-        ChatCompletionRequest request = requestOpenAI(
-                new com.theokanning.openai.completion.chat.ChatMessage("system", rolePrompt),
-                promptUser);
-        ChatCompletionResult chatCompletion = openAiService.createChatCompletion(request);
-        return chatCompletion.getChoices().get(0).getMessage().getContent();
-    }
-
-    @Override
-    public String generateDefensePrompt(Chat chat, SecurityUser securityUser, ChatMessageDto dto) {
-        User user = userRepository.findUserByEmail(securityUser.getUsername()).orElseThrow();
-        StringBuilder sb = new StringBuilder();
-        user.getLanguages().forEach(i -> sb.append(i.getName().getValue()).append(","));
-        String userLanguages = sb.toString();
-        sb.setLength(0);
-        user.getTechs().forEach(i -> sb.append(i.getName().getValue()).append(","));
-        String userTechs = sb.toString();
-        sb.setLength(0);
-        sb.append(DEFENSE_CHARACTER)
-                .append(user.getPosition().getValue())
-                .append(DEFENSE_ROLE)
-                .append(DEFENSE_ROLE_SITUATION)
-                .append(DEFENSE_ROLE_SITUATION_OPTION)
-                .append(user.getPosition().getValue())
-                .append("입니다. 사용 기술스택은")
-                .append(userLanguages)
-                .append(userTechs)
-                .append(" 입니다.")
-                .append(DEFENSE_ROLE_QUESTION_OPTION)
-                .append(DEFENSE_OPTION_QUESTION_FORBIDDEN_A)
-                .append(DEFENSE_OPTION_SCORE)
-                .append(DEFENSE_OPTION_SCORE_UNNATURAL);
-        String rolePrompt = sb.toString();
-
-        Optional<ChatMessage> optionalChatMessageByAIDesc = chatMessageRepository.findTopByChatOrderByCreatedAtDesc(chat);
-        String userPrompt;
-        userPrompt = optionalChatMessageByAIDesc
-                .map(chatMessage -> "당신이 질문한 내용인 [" + chatMessage.getMessage() + "]에 대한 저의 답변은 " + dto.message() + " 기술면접을 계속 진행해주시고, 연관된 질문을 1개만 해주세요.")
-                .orElse("");
-
-        ChatCompletionRequest request = requestOpenAI(new com.theokanning.openai.completion.chat.ChatMessage("system", rolePrompt), userPrompt);
-        ChatCompletionResult chatCompletion = openAiService.createChatCompletion(request);
-        chat.decreaseAIQuestionMaxNumber();
-        if (chat.isDefenseEnd()) {
-            String feedbackUser = "지금까지의 대화를 전문가의 입장에서 평가해서 보고서를 만들어주세요. 인터뷰가 끝났습니다.";
-            ChatCompletionRequest request1 = requestOpenAI(new com.theokanning.openai.completion.chat.ChatMessage("system", rolePrompt), feedbackUser);
-            ChatCompletionResult chatCompletion1 = openAiService.createChatCompletion(request1);
-            return "면접 디펜스가 끝났습니다. 감사합니다." + "\n" + chatCompletion1.getChoices().get(0).getMessage().getContent();
-        }
-        String content = chatCompletion.getChoices().get(0).getMessage().getContent();
-        Matcher matcher = SCORE_PATTERN.matcher(content);
-        if (matcher.find()) {
-            String scoreStr = matcher.group(1);
-            int score = Integer.parseInt(scoreStr);
-            chat.increaseScore(score);
-        }
-        return content;
     }
 
     private ChatCompletionRequest requestOpenAI(com.theokanning.openai.completion.chat.ChatMessage rolePrompt, String userPrompt) {
