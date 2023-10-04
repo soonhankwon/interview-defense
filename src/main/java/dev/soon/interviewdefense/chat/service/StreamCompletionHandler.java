d253380c9ad610e6ab05db1ed8bc056cd009312f
soonhankwon <soonable@gmail.com>
2023-10-04 19:05:45 +0900

    feat : gpt stream 핸들러 - 웹소켓 사용(proto)

--- src/main/java/dev/soon/interviewdefense/chat/service/StreamCompletionHandler.java
+++ src/main/java/dev/soon/interviewdefense/chat/service/StreamCompletionHandler.java
@@ -1,67 +1,64 @@
 package dev.soon.interviewdefense.chat.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.theokanning.openai.completion.chat.ChatCompletionRequest;
-import com.theokanning.openai.completion.chat.ChatMessage;
-import com.theokanning.openai.service.OpenAiService;
-import dev.soon.interviewdefense.chat.controller.dto.GPTCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatCompletionChunk;
+import dev.soon.interviewdefense.chat.domain.Chat;
+import dev.soon.interviewdefense.chat.domain.ChatMessage;
+import dev.soon.interviewdefense.chat.domain.ChatSender;
+import dev.soon.interviewdefense.chat.respository.ChatMessageRepository;
+import dev.soon.interviewdefense.chat.respository.ChatRepository;
+import io.reactivex.Flowable;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
-
-import java.util.HashMap;
-import java.util.List;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class StreamCompletionHandler extends TextWebSocketHandler {
 
-    private final HashMap<String, WebSocketSession> sessionHashMap;
     private final ObjectMapper objectMapper = new ObjectMapper();
-    private final OpenAiService openAiService;
-
-    // Client 접속시 호출되는 메서드
-    @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        sessionHashMap.put(session.getId(), session);
-        log.info("connected user={}", session.getId());
-    }
-
-    // Client 접속 해제시 호출되는 메서드
-    @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        sessionHashMap.remove(session.getId());
-        log.info("disconnected user={}", session.getId());
-    }
+    private final ChatMessageRepository chatMessageRepository;
+    private final ChatRepository chatRepository;
+    private final ChatService chatServiceV2;
 
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        GPTCompletionRequest completionRequest = objectMapper.readValue(message.getPayload(), GPTCompletionRequest.class);
-        sessionHashMap.keySet().forEach(key -> {
-            streamChatCompletion(key, completionRequest);
-        });
-    }
+        String payload = message.getPayload();
+        String[] payloadSegments = payload.split("%flag%");
+        Long chatId = Long.parseLong(payloadSegments[0]);
+        String userMessage = payloadSegments[1];
+        Chat chat = chatRepository.findById(chatId).orElseThrow();
+        chatMessageRepository.save(new ChatMessage(userMessage, chat, ChatSender.USER));
 
-    private void streamChatCompletion(String key, GPTCompletionRequest request) {
-        openAiService.streamChatCompletion(requestOpenAI(new ChatMessage("system", "프로그래밍 전문가"), request.getPrompt()))
-                .blockingForEach(completionChunk -> {
-                    sessionHashMap.get(key).sendMessage(new TextMessage(objectMapper.writeValueAsString(completionChunk)));
-                });
-    }
+        StringBuilder sb = new StringBuilder();
+        Flowable<ChatCompletionChunk> responseFlowable = chatServiceV2.generateStreamResponse(chat, userMessage);
 
-    private ChatCompletionRequest requestOpenAI(com.theokanning.openai.completion.chat.ChatMessage rolePrompt, String userPrompt) {
-        return ChatCompletionRequest.builder()
-                .model("gpt-3.5-turbo")
-                .messages(List.of(
-                        rolePrompt,
-                        new com.theokanning.openai.completion.chat.ChatMessage("user", userPrompt)
-                ))
-                .maxTokens(300)
-                .build();
+        // Flowable 을 구독하여 응답 스트림을 WebSocket 으로 전송
+        responseFlowable.subscribe(
+                chunk -> {
+                    try {
+                        String response = chunk.getChoices().get(0).getMessage().getContent();
+                        log.info("response={}", response);
+                        if(response == null) {
+                            return;
+                        }
+                        sb.append(response);
+                        session.sendMessage(new TextMessage(objectMapper.writeValueAsString(response)));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                },
+                Throwable::printStackTrace,
+                () -> {
+                    chatMessageRepository.save(new ChatMessage(sb.toString(), chat, ChatSender.AI));
+                    sb.setLength(0);
+                    // 응답 스트림이 완료될 때 실행할 코드
+                    // 여기에서 스트림 종료를 처리하거나 추가 작업을 수행할 수 있습니다.
+                }
+        );
     }
 }
