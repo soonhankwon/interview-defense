5c2a84576bbefc9b68d6d358e7babb1a77836581
soonhankwon <soonable@gmail.com>
2023-10-10 16:13:30 +0900

    refactor : 불필요 메서드 삭제

--- src/main/java/dev/soon/interviewdefense/chat/service/ChatService.java
+++ src/main/java/dev/soon/interviewdefense/chat/service/ChatService.java
@@ -1,7 +1,6 @@
 package dev.soon.interviewdefense.chat.service;
 
 import com.theokanning.openai.completion.chat.ChatCompletionChunk;
-import dev.soon.interviewdefense.chat.controller.dto.ChatMessageDto;
 import dev.soon.interviewdefense.chat.controller.dto.ChatRoomReqDto;
 import dev.soon.interviewdefense.chat.domain.Chat;
 import dev.soon.interviewdefense.chat.domain.ChatMessage;
@@ -14,10 +13,6 @@ public interface ChatService {
     Chat getChatRoom(SecurityUser securityUser, Long chatRoomId);
 
     Long createChatRoom(SecurityUser securityUser, ChatRoomReqDto dto);
-
-    Chat saveUserMessage(Long chatRoomId, SecurityUser securityUser, ChatMessageDto dto);
-
-    void saveAIMessage(Long chatRoomId, SecurityUser securityUser, String message);
 
     Flowable<ChatCompletionChunk> generateStreamResponse(Chat chat, String question);
 
