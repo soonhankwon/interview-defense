8a42af57933db374b396398ebd5917638e4ba6d8
soonhankwon <soonable@gmail.com>
2023-10-10 16:13:20 +0900

    refactor : 불필요 메서드 삭제

--- src/main/java/dev/soon/interviewdefense/chat/respository/ChatRepository.java
+++ src/main/java/dev/soon/interviewdefense/chat/respository/ChatRepository.java
@@ -3,7 +3,6 @@ package dev.soon.interviewdefense.chat.respository;
 import dev.soon.interviewdefense.chat.domain.Chat;
 import dev.soon.interviewdefense.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,12 +10,8 @@ import java.util.Optional;
 
 @Repository
 public interface ChatRepository extends JpaRepository<Chat, Long> {
+
     List<Chat> findChatsByUser(User user);
 
-    Optional<Chat> findChatByUserAndId(User user, Long chatId);
-
     Optional<Chat> findChatByIdAndUser(Long chatRoomId, User user);
-
-    @Query("SELECT c FROM Chat c WHERE c.mode = 'DEFENSE' AND c.aiQuestionsMaxNumber = 0 ORDER BY c.defenseScore DESC")
-    List<Chat> findClosedDefenseChatsOrderByScoreDesc();
 }
