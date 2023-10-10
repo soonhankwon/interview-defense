a9fac84865bcdf1c7790c832a2150de4431f21c4
soonhankwon <soonable@gmail.com>
2023-10-10 17:24:30 +0900

    refactor : 불필요 attribute 삭제

--- src/main/java/dev/soon/interviewdefense/chat/controller/ChatController.java
+++ src/main/java/dev/soon/interviewdefense/chat/controller/ChatController.java
@@ -44,10 +44,8 @@ public class ChatController {
         Chat chatRoom = chatServiceV2.getChatRoom(securityUser, chatRoomId);
         List<ChatMessage> chatMessagesInChatRoom = chatServiceV2.getChatRoomMessages(chatRoom);
         model.addAttribute("chatMessages", chatMessagesInChatRoom);
-        model.addAttribute("chat", chatRoom);
         User loginUserInfo = userService.getLoginUserInfo(securityUser);
         model.addAttribute("user", loginUserInfo);
-        model.addAttribute("chatMessageDto", new ChatMessageDto(null));
         return "chatRoom";
     }
 
