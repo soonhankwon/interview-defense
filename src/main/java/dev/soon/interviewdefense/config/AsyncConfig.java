2e0fcedb26d31590643abd28d0761829dcb53674
soonhankwon <soonable@gmail.com>
2023-10-17 12:12:10 +0900

    feat : Async 프록시 타겟 클래스 - true

--- src/main/java/dev/soon/interviewdefense/config/AsyncConfig.java
+++ src/main/java/dev/soon/interviewdefense/config/AsyncConfig.java
@@ -10,7 +10,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import java.util.concurrent.Executor;
 
 @Configuration
-@EnableAsync
+@EnableAsync(proxyTargetClass = true)
 public class AsyncConfig extends AsyncConfigurerSupport {
 
     @Override
