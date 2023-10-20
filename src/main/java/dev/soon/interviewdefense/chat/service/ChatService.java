11c52f8716f2dd81991ead2c204ea7607a0f7022
soonhankwon <soonable@gmail.com>
2023-10-20 18:10:21 +0900

    refactor : email 문자열로 조회하는 메서드로 변경 (시큐리티와 디커플링)

--- src/main/java/dev/soon/interviewdefense/chat/service/ChatService.java
+++ src/main/java/dev/soon/interviewdefense/chat/service/ChatService.java
@@ -4,19 +4,18 @@ import com.theokanning.openai.completion.chat.ChatCompletionChunk;
 import dev.soon.interviewdefense.chat.controller.dto.ChatRoomReqDto;
 import dev.soon.interviewdefense.chat.domain.Chat;
 import dev.soon.interviewdefense.chat.domain.ChatMessage;
-import dev.soon.interviewdefense.security.SecurityUser;
 import io.reactivex.Flowable;
 
 import java.util.List;
 
 public interface ChatService {
-    Chat getChatRoom(SecurityUser securityUser, Long chatRoomId);
+    Chat getChatRoom(String email, Long chatRoomId);
 
-    Long createChatRoom(SecurityUser securityUser, ChatRoomReqDto dto);
+    Long createChatRoom(String email, ChatRoomReqDto dto);
 
     Flowable<ChatCompletionChunk> generateStreamResponse(Chat chat, String question);
 
-    void deleteChat(Long chatRoomId, SecurityUser securityUser);
+    void deleteChat(Long chatRoomId, String email);
 
     List<ChatMessage> getChatRoomMessages(Chat chatRoom);
 }
