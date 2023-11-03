bcecd1970f6c065a9d78adaa5b493799a8f7b49e
soonhankwon <soonable@gmail.com>
2023-11-03 11:34:27 +0900

    refactor: 불필요 쿼리 메서드 삭제

--- src/main/java/dev/soon/interviewdefense/user/repository/UserLanguageRepository.java
+++ src/main/java/dev/soon/interviewdefense/user/repository/UserLanguageRepository.java
@@ -1,17 +1,14 @@
 package dev.soon.interviewdefense.user.repository;
 
-import dev.soon.interviewdefense.user.domain.UserLanguage;
 import dev.soon.interviewdefense.user.domain.Language;
 import dev.soon.interviewdefense.user.domain.User;
+import dev.soon.interviewdefense.user.domain.UserLanguage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserLanguageRepository extends JpaRepository<UserLanguage, Long> {
-    List<UserLanguage> findUserLanguagesByUser(User user);
-    Optional<UserLanguage> findUserLanguageByIdAndUser(Long languageId, User user);
-    boolean existsUserLanguageByUserAndLanguage(User user, Language language);
+    Optional<UserLanguage> findUserLanguageByUserAndLanguage(User user, Language language);
 }
