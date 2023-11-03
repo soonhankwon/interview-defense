93d8d868e5d3796350f71e206f6c18d8217d34d9
soonhankwon <soonable@gmail.com>
2023-11-03 11:31:57 +0900

    refactor: 불필요 쿼리 메서드 삭제

--- src/main/java/dev/soon/interviewdefense/user/repository/UserTechRepository.java
+++ src/main/java/dev/soon/interviewdefense/user/repository/UserTechRepository.java
@@ -1,17 +1,14 @@
 package dev.soon.interviewdefense.user.repository;
 
-import dev.soon.interviewdefense.user.domain.UserTech;
 import dev.soon.interviewdefense.user.domain.Tech;
 import dev.soon.interviewdefense.user.domain.User;
+import dev.soon.interviewdefense.user.domain.UserTech;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserTechRepository extends JpaRepository<UserTech, Long> {
-    List<UserTech> findUserTechesByUser(User user);
-    Optional<UserTech> findUserTechByIdAndUser(Long techId, User user);
-    boolean existsByUserAndTech(User user, Tech tech);
+    Optional<UserTech> findUserTechByUserAndTech(User user, Tech tech);
 }
