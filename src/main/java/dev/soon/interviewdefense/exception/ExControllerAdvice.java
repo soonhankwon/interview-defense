e7dd19e178e16822e6a254c493856a83ed8c33ea
soonhankwon <soonable@gmail.com>
2023-11-03 11:02:22 +0900

    feat: ExceptionControllerAdvice

--- src/main/java/dev/soon/interviewdefense/exception/ExControllerAdvice.java
+++ src/main/java/dev/soon/interviewdefense/exception/ExControllerAdvice.java
@@ -1,25 +1,20 @@
 package dev.soon.interviewdefense.exception;
 
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@Slf4j
 @ControllerAdvice
 public class ExControllerAdvice {
 
-    @ExceptionHandler({IllegalArgumentException.class})
-    public void handleIllegalArgEx(IllegalArgumentException ex, HttpServletResponse response) throws IOException {
-        log.error("error message={}", ex.getMessage());
-        response.sendError(404);
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResult handleIllegalArgEx(IllegalArgumentException ex) {
+        return new ErrorResult(HttpStatus.BAD_REQUEST, ex.getMessage());
     }
 
-    @ExceptionHandler({ApiException.class})
-    public void handleApiEx(ApiException ex, HttpServletResponse response) throws IOException {
-        log.error("error message={}", ex.getCustomErrorCode().getMessage());
-        response.sendError(400);
+    @ExceptionHandler(ApiException.class)
+    public ErrorResult handleApiEx(ApiException ex) {
+        CustomErrorCode customErrorCode = ex.getCustomErrorCode();
+        return new ErrorResult(customErrorCode.getHttpStatus(), customErrorCode.getMessage());
     }
 }
