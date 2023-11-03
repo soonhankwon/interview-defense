2cc741496d9929aa3810e92a41e3b00cb441c06f
soonhankwon <soonable@gmail.com>
2023-11-03 10:45:08 +0900

    feat: RestAPI로 리팩토링 -> 불필요 클래스 삭제

--- src/main/java/dev/soon/interviewdefense/web/dto/MyLanguageReqDto.java
+++
@@ -1,6 +0,0 @@
-package dev.soon.interviewdefense.web.dto;
-
-import dev.soon.interviewdefense.user.domain.Language;
-
-public record MyLanguageReqDto(Language name) {
-}
--- src/main/java/dev/soon/interviewdefense/web/dto/MyTechReqDto.java
+++
@@ -1,6 +0,0 @@
-package dev.soon.interviewdefense.web.dto;
-
-import dev.soon.interviewdefense.user.domain.Tech;
-
-public record MyTechReqDto(Tech name) {
-}
