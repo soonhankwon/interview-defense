dc0919e9cf3183cc92d192105b710fc195d58a9c
soonhankwon <soonable@gmail.com>
2023-11-07 13:39:43 +0900

    doc: Oauth 관련 API Doc 추가(#35)

--- src/main/java/dev/soon/interviewdefense/oauth/controller/OauthController.java
+++ src/main/java/dev/soon/interviewdefense/oauth/controller/OauthController.java
@@ -5,26 +5,29 @@ import dev.soon.interviewdefense.oauth.controller.dto.NaverOauthLoginParam;
 import dev.soon.interviewdefense.oauth.service.OauthService;
 import dev.soon.interviewdefense.security.service.JwtService;
 import dev.soon.interviewdefense.user.domain.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
+@Tag(name = "Oauth2 관련 API")
 public class OauthController {
 
     private final OauthService<KakaoOauthLoginParam> kakaoOauthService;
     private final OauthService<NaverOauthLoginParam> naverOauthService;
     private final JwtService jwtService;
 
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "oauth & Header JWT 발급 API")
     @GetMapping("/oauth/{provider}")
     public ResponseEntity<String> oauthCallback(@PathVariable String provider, HttpServletResponse response,
                                                 @RequestParam String code,
